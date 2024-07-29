09f171ed2a6a2d9487913cdd5c6d08a394c9d6fa
Matt Newman <matthew.newman@cloudcall.com>
2024-07-30 00:28:33 +0100

    fixing typo of names for example user pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <ul>
-        <li><a href="/customer/3dfdc5cf-b8ed-40f7-a5ca-5e88ad103b60">Kayleigh</a></li>
-        <li><a href="/customer/ff535484-6880-4653-b06e-89983ecf4ed5">Wilhelmine</a></li>
+        <li><a href="/customer/3dfdc5cf-b8ed-40f7-a5ca-5e88ad103b60">Wilhelmine</a></li>
+        <li><a href="/customer/ff535484-6880-4653-b06e-89983ecf4ed5">Kayleigh</a></li>
       </ul>
     </main>
   );
